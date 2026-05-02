93e1f6c00e9b8ade7b6ead8f2234bdc4bbf3fd31
jbzz-tech <anonimomods01@gmail.com>
2026-05-02 12:28:58 -0300

    Desabilita ProGuard, fundo preto, texto branco gigante

--- app/src/main/java/com/exemplo/app/MainActivity.kt
+++ app/src/main/java/com/exemplo/app/MainActivity.kt
@@ -3,15 +3,18 @@ package com.exemplo.app
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.TextView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val textView = TextView(this)
-        textView.text = "Template 🐴 Kotlin"
-        textView.textSize = 24f
-        textView.setTextColor(Color.BLACK)
+        textView.text = "🐴 FUNCIONOU! 🐴"
+        textView.textSize = 28f
+        textView.setTextColor(Color.WHITE)
+        textView.setBackgroundColor(Color.BLACK)
+        textView.gravity = Gravity.CENTER
         setContentView(textView)
     }
 }
