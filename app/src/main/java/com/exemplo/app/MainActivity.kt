0b5ec6a939198ed7d03096912f77577dd95847cb
jbzz-tech <anonimomods01@gmail.com>
2026-05-02 14:22:43 -0300

    Template Kotlin moderno

--- app/src/main/java/com/exemplo/app/MainActivity.kt
+++ app/src/main/java/com/exemplo/app/MainActivity.kt
@@ -1,20 +1,12 @@
 package com.exemplo.app
 
-import android.app.Activity
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textView = TextView(this)
-        textView.text = "🐴 FUNCIONOU! 🐴"
-        textView.textSize = 28f
-        textView.setTextColor(Color.WHITE)
-        textView.setBackgroundColor(Color.BLACK)
-        textView.gravity = Gravity.CENTER
-        setContentView(textView)
     }
+
 }
